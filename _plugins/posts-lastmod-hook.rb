abf334874935427e1c629bca6e8a2bc2696bc9d8
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-01-14 10:53:24 +0100

    Comment debug code

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -9,8 +9,8 @@ Jekyll::Hooks.register :posts, :post_init do |post|
   if commit_num.to_i > 1
     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
     post.data['last_modified_at'] = lastmod_date
-    Jekyll.logger.info "Post URL: #{post.site.config['url']}#{post.url}"
-    Jekyll.logger.info "Post updated: #{post.data['last_modified_at']}"
+    #Jekyll.logger.info "Post URL: #{post.site.config['url']}#{post.url}"
+    #Jekyll.logger.info "Post updated: #{post.data['last_modified_at']}"
   end
 
 end

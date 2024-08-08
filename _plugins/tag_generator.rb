3373152456857faf468112a9e8873bc29164b3ab
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-08-08 22:25:47 +0200

    Implement tag autogenerated pages, fix collections page style

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,58 @@
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+    priority :low
+
+    def generate(site)
+      tags = Hash.new { |hash, key| hash[key] = [] }
+
+      # Collect tags from posts
+      site.posts.docs.each do |post|
+        post_tags = post.data['tags'] || []
+        post_tags.each do |tag|
+          normalized_tag = normalize_tag(tag)
+          tags[normalized_tag] << post
+        end
+      end
+
+      # Collect tags from notes (assuming notes are in a separate collection)
+      if site.collections['notes']
+        site.collections['notes'].docs.each do |note|
+          note_tags = note.data['tags'] || []
+          note_tags.each do |tag|
+            normalized_tag = normalize_tag(tag)
+            tags[normalized_tag] << note
+          end
+        end
+      end
+
+      tags.each do |tag, items|
+        page = TagPage.new(site, tag, items)
+        site.pages << page
+      end
+    end
+
+    private
+
+    def normalize_tag(tag)
+      tag.strip.downcase.gsub(/[^a-z0-9\s-]/, '').gsub(/\s+/, '-')
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, tag_slug, items)
+      @site = site
+      @base = site.source
+      @dir = "tags"
+      @name = "#{tag_slug}.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(@base, '_layouts'), 'tag.html')
+      self.data['title'] = "#{tag_slug}"
+      self.data['description'] = "List of posts and notes tagged with #{tag_slug}"
+      self.data['tag'] = tag_slug
+      self.data['items'] = items
+      self.data['permalink'] = "/tags/#{tag_slug}/"
+    end
+  end
+end
--- pages/collections.md
+++ pages/collections.md
@@ -1,10 +1,15 @@
 ---
 permalink: /collections
 layout: Post
+format: list
 content-type: static
 title: Collections
 description: A curated collection of valuable resources, aggregated for convenience.
 sitemap: false
 ---
+
+<br>
+
+<p>{{ page.description }}</p>
 
 {%- include Collection.html -%}

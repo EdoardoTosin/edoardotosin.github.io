a35c2a2c3f75df97c89c2d23e56807f0c5ba4fbd
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2026-06-07 12:22:54 +0200

    fix(defang): broaden dot variants, fix empty-bracket bug, strip trailing punctuation

--- _plugins/defang.rb
+++ _plugins/defang.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
-# defang.rb - Wraps defanged URL/IP notation (hXXp/hxxp, [.], [dot]) in <span class="defanged">
+# defang.rb - Wraps defanged URL/IP notation (hXXp/hxxp, [.], (.), [dot], (dot)) in <span class="defanged">
 # at :post_convert; skips code/pre/a/tag boundaries so only prose text is affected.
 
 module Defang
 
   SKIP_RE   = /(<pre[\s>].*?<\/pre>|<code[\s>].*?<\/code>|<a[\s>].*?<\/a>|<script[\s>].*?<\/script>|<style[\s>].*?<\/style>|<!--.*?-->|<[^>]+>)/mi.freeze
-  SCHEME_RE = /\bh[Xx]{2}ps?(?:\[?[:\/.]+\]?)+\S*/.freeze
-  DOT_RE    = /\b[\w.\-]+(?:\[\.?\]|\[dot\])[\w.\-\[\].]*\b/.freeze
+  SCHEME_RE = /\bh[Xx]{2}ps?(?:\[?[:\/.]+\]?)+\S*(?<![.,;:!?)\]])/.freeze
+  DOT_RE    = /\b[\w.\-]+(?:\[\.\]|\[dot\]|\(\.\)|\(dot\))(?:[\w.\-]+|\[\.\]|\[dot\]|\(\.\)|\(dot\))*\b/i.freeze
   DEFANG_RE = Regexp.union(SCHEME_RE, DOT_RE).freeze
 
   def self.process(html)
@@ -21,7 +21,7 @@ end
 Jekyll::Hooks.register :documents, :post_convert do |doc|
   next unless doc.output_ext == '.html'
   next unless doc.content.is_a?(String) && !doc.content.empty?
-  next unless doc.content.match?(/h[Xx]{2}p|\[\.\]|\[dot\]/i)
+  next unless doc.content.match?(/h[Xx]{2}p|\[\.\]|\[dot\]|\(\.\)|\(dot\)/i)
 
   doc.content = Defang.process(doc.content)
 end
